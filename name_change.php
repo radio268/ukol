a7658c2bc216402053cfba68a578c1e3f4a6e4db
radio268 <144229001+radio268@users.noreply.github.com>
2024-04-24 12:20:31 +0200

    Update name_change.php

--- name_change.php
+++ name_change.php
@@ -1,10 +1,11 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
     
-    if ( isset( $_COOKIE['text'] )){
-        $text_in = $_COOKIE['text'];
-    }
-    else{
+    if ( isset( $_COOKIE['text'] ))
+        {$text_in = $_COOKIE['text'];}
+    else
+    {
         setcookie( 'text' , "" , 0 , "/" );
         $text_in = $_COOKIE['text'];
     }  
@@ -63,18 +64,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $text_out = $text_in;
     
-    if (isset($_POST['type'])){
+    if (isset($_POST['type']))
+    {
         $searchPattern = '/\b(' . implode('|', array_map('preg_quote', $list1)) . ')\b/';
         $text_out = preg_replace($searchPattern, implode(' ', $list2), $text_in);
     }
-    else{
-        $text_out = str_replace($list1, $list2, $text_out);
-    }
+    else
+        {$text_out = str_replace($list1, $list2, $text_out);}
 
     echo $text_out;
-    $age = $_COOKIE['age'];
+    
+    if (isset($_COOKIE['age']))
+        {$age = $_COOKIE['age'];}
+    else
+        {$age = 0;}
     
     setcookie( 'text' , $text_out  , $age , "/" );
+    
 }?>
 
 
