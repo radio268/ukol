f7c88180bde428c5a2766d538b54cf852a922d32
radio268 <144229001+radio268@users.noreply.github.com>
2024-04-24 08:33:15 +0200

    code

--- name_change.php
+++ name_change.php
@@ -26,6 +26,16 @@
 
     <!-- main content  -------------------------------------------------------------------------------------------->
     <main>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <p>
+                <input type="string" name="text_in" id="text_in"><br>
+
+                what do you want to do with it:
+                <textarea name="selection_1" id="selection_1" rows="4"></textarea>
+
+                <input type="submit" value="go"><br>
+            </p>
+        </form> 
 
 
     
